6f210c143578981992cec465e567c70f990378ce
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-10-27 00:44:48 +0200

    Add example plugin code

---
+++ example/MyShop.cs
@@ -0,0 +1,38 @@
+﻿using ChatAIze.PluginApi.Enums;
+using ChatAIze.PluginApi.Interfaces;
+
+namespace ChatAIze.PluginApi.ExamplePlugin;
+
+public class MyShop : IPluginLoader
+{
+    public ValueTask<IChatbotPlugin> LoadAsync()
+    {
+        var getProducts = new ChatbotFunction
+        {
+            Id = new("a546aca6-5280-4c18-a339-b216588a5d12"),
+            Name = "get_order_status",
+            Description = "Gets the status of an order with the given order id.",
+            Parameters =
+            [
+                new FunctionParameter
+                {
+                    Name = "id",
+                    Type = ParameterType.Text,
+                    Description = "The category of the products",
+                }
+            ],
+            Callback = () => ValueTask.FromResult<object?>("Order status: shipped")
+        };
+
+        var plugin = new ChatbotPlugin
+        {
+            Id = new("55bc120a-b623-4d5f-91e6-ae2b9f3bf6e2"),
+            Name = "MyShop",
+            Description = "A simple shop plugin",
+            Version = "1.0.0",
+            Functions = [getProducts]
+        };
+
+        return ValueTask.FromResult<IChatbotPlugin>(plugin);
+    }
+}
