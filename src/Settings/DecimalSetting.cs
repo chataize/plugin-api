70e11433583a6c27ec86c929863befd3195d8812
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-08 16:22:16 +0100

    Add DecimalSetting class constructors

--- src/Settings/DecimalSetting.cs
+++ src/Settings/DecimalSetting.cs
@@ -1,9 +1,28 @@
+using System.Diagnostics.CodeAnalysis;
 using ChatAIze.Abstractions.Settings;
 
 namespace ChatAIze.PluginApi.Settings;
 
 public class DecimalSetting : IDecimalSetting
 {
+    public DecimalSetting() { }
+
+    [SetsRequiredMembers]
+    public DecimalSetting(string key, string? title = null, string? description = null, double defaultValue = 0.0, double minValue = double.MinValue, double maxValue = double.MaxValue, bool showSliderValue = true, bool showSliderPercentage = false, string? minValueLabel = null, string? maxValueLabel = null, bool isDisabled = false)
+    {
+        Key = key;
+        Title = title;
+        Description = description;
+        DefaultValue = defaultValue;
+        MinValue = minValue;
+        MaxValue = maxValue;
+        ShowSliderValue = showSliderValue;
+        ShowSliderPercentage = showSliderPercentage;
+        MinValueLabel = minValueLabel;
+        MaxValueLabel = maxValueLabel;
+        IsDisabled = isDisabled;
+    }
+
     public virtual required string Key { get; set; }
 
     public virtual string? Title { get; set; }
@@ -16,7 +35,7 @@ public class DecimalSetting : IDecimalSetting
 
     public virtual double MaxValue { get; set; } = double.MaxValue;
 
-    public virtual bool ShowSliderValue { get; set; }
+    public virtual bool ShowSliderValue { get; set; } = true;
 
     public virtual bool ShowSliderPercentage { get; set; }
 
