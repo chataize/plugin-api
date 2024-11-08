00d068567b3bed2422f675dc78d815b26bec9edd
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-08 16:47:26 +0100

    Add SelectionSetting class constructors

--- src/Settings/SelectionSetting.cs
+++ src/Settings/SelectionSetting.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using ChatAIze.Abstractions.Settings;
 using ChatAIze.Abstractions.UI;
 
@@ -5,6 +6,20 @@ namespace ChatAIze.PluginApi.Settings;
 
 public class SelectionSetting : ISelectionSetting
 {
+    public SelectionSetting() { }
+
+    [SetsRequiredMembers]
+    public SelectionSetting(string key, string? title = null, string? description = null, SelectionSettingStyle style = SelectionSettingStyle.Automatic, string? defaultValue = null, bool isCompact = false, bool isDisabled = false)
+    {
+        Key = key;
+        Title = title;
+        Description = description;
+        Style = style;
+        DefaultValue = defaultValue;
+        IsCompact = isCompact;
+        IsDisabled = isDisabled;
+    }
+
     public virtual required string Key { get; set; }
 
     public virtual string? Title { get; set; }
@@ -13,7 +28,7 @@ public class SelectionSetting : ISelectionSetting
 
     public virtual SelectionSettingStyle Style { get; set; }
 
-    public required virtual string DefaultValue { get; set; }
+    public virtual string? DefaultValue { get; set; }
 
     public virtual ICollection<ISelectionChoice> Choices { get; set; } = [];
 
