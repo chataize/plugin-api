f8f086be4c8a762eb1530f499b3be10800fcad36
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-08 16:19:36 +0100

    Add DateTimeSetting class constructors

--- src/Settings/DateTimeSetting.cs
+++ src/Settings/DateTimeSetting.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using ChatAIze.Abstractions.Settings;
 using ChatAIze.Abstractions.UI;
 
@@ -5,6 +6,21 @@ namespace ChatAIze.PluginApi.Settings;
 
 public class DateTimeSetting : IDateTimeSetting
 {
+    public DateTimeSetting() { }
+
+    [SetsRequiredMembers]
+    public DateTimeSetting(string key, string? title = null, string? description = null, DateTimeSettingStyle style = DateTimeSettingStyle.DateTime, DateTimeOffset? defaultValue = null, DateTimeOffset? minValue = null, DateTimeOffset? maxValue = null, bool isDisabled = false)
+    {
+        Key = key;
+        Title = title;
+        Description = description;
+        Style = style;
+        DefaultValue = defaultValue ?? DateTimeOffset.UtcNow;
+        MinValue = minValue ?? DateTimeOffset.MinValue;
+        MaxValue = maxValue ?? DateTimeOffset.MaxValue;
+        IsDisabled = isDisabled;
+    }
+
     public virtual required string Key { get; set; }
 
     public virtual string? Title { get; set; }
