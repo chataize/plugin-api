b880b18f9d97c7121d254fb6679bd5b1ae044d7b
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-08 16:54:47 +0100

    Add SettingsParagraph class constructors

--- src/Settings/SettingsParagraph.cs
+++ src/Settings/SettingsParagraph.cs
@@ -1,9 +1,20 @@
+using System.Diagnostics.CodeAnalysis;
 using ChatAIze.Abstractions.Settings;
 
 namespace ChatAIze.PluginApi.Settings;
 
 public class SettingsParagraph : ISettingsParagraph
 {
+    public SettingsParagraph() { }
+
+    [SetsRequiredMembers]
+    public SettingsParagraph(string key, string? content = null, bool isDisabled = false)
+    {
+        Key = key;
+        Content = content;
+        IsDisabled = isDisabled;
+    }
+
     public virtual required string Key { get; set; }
 
     public virtual string? Content { get; set; }
