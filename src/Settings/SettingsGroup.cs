a2d3ff2b5f72f1ff73b0b5a1f9fe480b6e4073d9
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-08 16:52:41 +0100

    Add SettingsGroup class constructors

--- src/Settings/SettingsGroup.cs
+++ src/Settings/SettingsGroup.cs
@@ -1,9 +1,22 @@
+using System.Diagnostics.CodeAnalysis;
 using ChatAIze.Abstractions.Settings;
 
 namespace ChatAIze.PluginApi.Settings;
 
 public class SettingsGroup : ISettingsGroup
 {
+    public SettingsGroup() { }
+
+    [SetsRequiredMembers]
+    public SettingsGroup(string key, string? title = null, string? description = null, bool isDisabled = false, ICollection<IPluginSetting>? settings = null)
+    {
+        Key = key;
+        Title = title;
+        Description = description;
+        IsDisabled = isDisabled;
+        Settings = settings ?? [];
+    }
+
     public virtual required string Key { get; set; }
 
     public virtual string? Title { get; set; }
@@ -12,5 +25,5 @@ public class SettingsGroup : ISettingsGroup
 
     public virtual bool IsDisabled { get; set; }
 
-    public virtual required ICollection<IPluginSetting> Settings { get; set; }
+    public virtual ICollection<IPluginSetting> Settings { get; set; } = [];
 }
