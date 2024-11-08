59de50921ce173143582c05d4e70fbb79d649a48
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-08 16:56:33 +0100

    Add SettingsSection class constructors

--- src/Settings/SettingsSection.cs
+++ src/Settings/SettingsSection.cs
@@ -1,9 +1,22 @@
+using System.Diagnostics.CodeAnalysis;
 using ChatAIze.Abstractions.Settings;
 
 namespace ChatAIze.PluginApi.Settings;
 
 public class SettingsSection : ISettingsSection
 {
+    public SettingsSection() { }
+
+    [SetsRequiredMembers]
+    public SettingsSection(string key, string? title = null, string? description = null, bool isDisabled = false, ICollection<IPluginSetting>? settings = null)
+    {
+        Key = key;
+        Title = title;
+        Description = description;
+        IsDisabled = isDisabled;
+        Settings = settings ?? [];
+    }
+
     public virtual required string Key { get; set; }
 
     public virtual string? Title { get; set; }
