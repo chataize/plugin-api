bbb0c94657b41a630bfc6b3c2d514ac72e66f075
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-08 23:45:38 +0100

    Reorganize ChatFunction class members

--- src/ChatFunction.cs
+++ src/ChatFunction.cs
@@ -8,30 +8,21 @@ public class ChatFunction : IChatFunction
     public ChatFunction() { }
 
     [SetsRequiredMembers]
-    public ChatFunction(string name, string? description = null, ICollection<IFunctionParameter>? parameters = null, Delegate? callback = null)
+    public ChatFunction(string name, string? description = null, Delegate? callback = null, ICollection<IFunctionParameter>? parameters = null)
     {
         Name = name;
         Description = description;
-        Parameters = parameters;
         Callback = callback;
+        Parameters = parameters;
     }
-
-    [SetsRequiredMembers]
-    public ChatFunction(string name, Delegate? callback = null) : this(name, null, null, callback) { }
-
-    [SetsRequiredMembers]
-    public ChatFunction(string name, string? description = null, Delegate? callback = null) : this(name, description, null, callback) { }
-
-    [SetsRequiredMembers]
-    public ChatFunction(string name, ICollection<IFunctionParameter>? parameters = null, Delegate? callback = null) : this(name, null, parameters, callback) { }
 
     public virtual required string Name { get; set; }
 
     public virtual string? Description { get; set; }
 
-    public virtual ICollection<IFunctionParameter>? Parameters { get; set; }
-
     public virtual bool RequiresDoubleCheck { get; set; }
 
     public virtual Delegate? Callback { get; set; }
+
+    public virtual ICollection<IFunctionParameter>? Parameters { get; set; }
 }
