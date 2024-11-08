e16ceff138cf31f569fd7706353ba996b44b0a59
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-08 16:31:23 +0100

    Add SelectionChoice class constructors

--- src/Settings/SelectionChoice.cs
+++ src/Settings/SelectionChoice.cs
@@ -1,9 +1,20 @@
+using System.Diagnostics.CodeAnalysis;
 using ChatAIze.Abstractions.Settings;
 
 namespace ChatAIze.PluginApi.Settings;
 
 public class SelectionChoice : ISelectionChoice
 {
+    public SelectionChoice() { }
+
+    [SetsRequiredMembers]
+    public SelectionChoice(string value, string? title = null, bool isDisabled = false)
+    {
+        Value = value;
+        Title = title;
+        IsDisabled = isDisabled;
+    }
+
     public virtual string? Title { get; set; }
 
     public virtual required string Value { get; set; }
