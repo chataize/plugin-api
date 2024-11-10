8de82b6bac91e4e158227360eced13c52d12ddac
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-10 16:45:32 +0100

    Add Settings and Functions properties to ChatbotPlugin class

--- src/ChatbotPlugin.cs
+++ src/ChatbotPlugin.cs
@@ -7,10 +7,14 @@ namespace ChatAIze.PluginApi;
 
 public class ChatbotPlugin : IChatbotPlugin
 {
-    public ChatbotPlugin() { }
+    public ChatbotPlugin()
+    {
+        SettingsCallback ??= (_, _) => ValueTask.FromResult(Settings);
+        FunctionsCallback ??= (_, _) => ValueTask.FromResult(Functions);
+    }
 
     [SetsRequiredMembers]
-    public ChatbotPlugin(Guid id, string title, string? description = null, string? website = null, string? author = null, string version = "1.0.0", DateTimeOffset? releaseTime = null, DateTimeOffset? lastUpdateTime = null, Func<IPluginSettings, CancellationToken, ValueTask<ICollection<IPluginSetting>>>? settingsCallback = null, Func<IPluginSettings, CancellationToken, ValueTask<ICollection<IChatFunction>>>? functionsCallback = null, IDictionary<string, Func<IDictionary<string, object?>?, CancellationToken, ValueTask<object?>>>? sharedMethods = null)
+    public ChatbotPlugin(Guid id, string title, string? description = null, string? website = null, string? author = null, string version = "1.0.0", DateTimeOffset? releaseTime = null, DateTimeOffset? lastUpdateTime = null, ICollection<IPluginSetting>? settings = null, ICollection<IChatFunction>? functions = null, Func<IPluginSettings, CancellationToken, ValueTask<ICollection<IPluginSetting>>>? settingsCallback = null, Func<IPluginSettings, CancellationToken, ValueTask<ICollection<IChatFunction>>>? functionsCallback = null, IDictionary<string, Func<IDictionary<string, object?>?, CancellationToken, ValueTask<object?>>>? sharedMethods = null)
     {
         Id = id;
         Title = title;
@@ -20,8 +24,27 @@ public class ChatbotPlugin : IChatbotPlugin
         Version = version;
         ReleaseTime = releaseTime;
         LastUpdateTime = lastUpdateTime;
-        SettingsCallback = settingsCallback;
-        FunctionsCallback = functionsCallback;
+        Settings = settings ?? [];
+        Functions = functions ?? [];
+
+        if (settingsCallback is null)
+        {
+            SettingsCallback = (_, _) => ValueTask.FromResult(Settings);
+        }
+        else
+        {
+            SettingsCallback = settingsCallback;
+        }
+
+        if (functionsCallback is null)
+        {
+            FunctionsCallback = (_, _) => ValueTask.FromResult(Functions);
+        }
+        else
+        {
+            FunctionsCallback = functionsCallback;
+        }
+
         SharedMethods = sharedMethods ?? new Dictionary<string, Func<IDictionary<string, object?>?, CancellationToken, ValueTask<object?>>>();
     }
 
@@ -42,6 +65,10 @@ public class ChatbotPlugin : IChatbotPlugin
     public virtual DateTimeOffset? LastUpdateTime { get; set; }
 
     public IDictionary<string, Func<IDictionary<string, object?>?, CancellationToken, ValueTask<object?>>> SharedMethods { get; set; } = new Dictionary<string, Func<IDictionary<string, object?>?, CancellationToken, ValueTask<object?>>>();
+
+    public virtual ICollection<IPluginSetting> Settings { get; set; } = [];
+
+    public virtual ICollection<IChatFunction> Functions { get; set; } = [];
 
     public virtual Func<IPluginSettings, CancellationToken, ValueTask<ICollection<IPluginSetting>>>? SettingsCallback { get; set; }
 
