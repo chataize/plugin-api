b5a02655abc3fe6f99f7630dd897dfe358eb82ac
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-11 18:01:15 +0100

    Add FunctionAction class

---
+++ src/Actions/FunctionAction.cs
@@ -0,0 +1,38 @@
+using ChatAIze.Abstractions.Actions;
+using ChatAIze.Abstractions.Settings;
+
+namespace ChatAIze.PluginApi.Actions;
+
+public class FunctionAction : IFunctionAction
+{
+    public FunctionAction() { }
+
+    public FunctionAction(string key, string title, Func<IDictionary<string, object>, ValueTask<object>>? callback = null, ICollection<IPluginSetting>? settings = null)
+    {
+        Key = key;
+        Title = title;
+        Callback = callback;
+        Settings = settings ?? [];
+    }
+
+    public FunctionAction(string key, string title, Func<IDictionary<string, object>, ValueTask<object>>? callback = null, params IPluginSetting[] settings)
+    {
+        Key = key;
+        Title = title;
+        Callback = callback;
+        Settings = settings ?? [];
+    }
+
+    public virtual required string Key { get; set; }
+
+    public virtual required string Title { get; set; }
+
+    public virtual Func<IDictionary<string, object>, ValueTask<object>>? Callback { get; set; }
+
+    public virtual ICollection<IPluginSetting> Settings { get; set; } = [];
+
+    public void AddSetting(IPluginSetting setting)
+    {
+        Settings.Add(setting);
+    }
+}
