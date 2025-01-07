5dbc021b6ca5449e3297a9e81b4ad6e29d396d71
Marcel Kwiatkowski <marcel2215@icloud.com>
2025-01-07 21:00:06 +0100

    Add DatabaseSorting record

---
+++ src/Databases/DatabaseSorting.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics.CodeAnalysis;
+using ChatAIze.Abstractions.Databases;
+
+namespace ChatAIze.PluginApi.Databases;
+
+public record DatabaseSorting : IDatabaseSorting
+{
+    public DatabaseSorting() { }
+
+    [SetsRequiredMembers]
+    public DatabaseSorting(string property, SortOrder order)
+    {
+        Property = property;
+        Order = order;
+    }
+
+    public virtual required string Property { get; set; }
+
+    public virtual SortOrder Order { get; set; }
+}
